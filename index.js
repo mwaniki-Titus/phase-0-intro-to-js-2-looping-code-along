1cc3396bdd771403a3fa309a852d6e909c58d06f
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-06-16 08:27:21 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+// Code your solutions in this file 
+const names=["Guadalupe","Ollie", "Aki"]
+const event="wordeful suprise";
+function writeCards(name,event) {
+    const thankYouMssages=[];
+    for(let i = 0;i< names.length;i++){
+        thankYouMssages.push("Thank you,${names[i]}, for the ${event}gift!");
+  
+    }
+    return thankYouMssages;
+    
+}
+function countDown(number) {
+    while (number>=0){
+        console.log(number);
+        number--;
+    }
+    
+}
